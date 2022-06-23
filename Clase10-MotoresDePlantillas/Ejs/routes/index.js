c10a670be167aa15e476e5d584ac3155f8356ab3
Santiago Güerci <santiago.38@hotmail.com>
2022-06-23 17:23:19 -0300

    Ejs resuelto

---
+++ Clase10-MotoresDePlantillas/Ejs/routes/index.js
@@ -0,0 +1,11 @@
+const {Router} = require('express');
+const router = Router();
+const { getFormulario, getProductos, addProducto } = require('../controllers/productsController');
+
+router.get("/", getFormulario);
+
+router.get("/productos", getProductos)
+
+router.post("/productos", addProducto)
+
+module.exports = router;
