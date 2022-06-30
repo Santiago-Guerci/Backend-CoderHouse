4bb4cc12c4717df46e9f0c16b606dcfaa0b11c19
Santiago Güerci <santiago.38@hotmail.com>
2022-06-29 21:22:14 -0300

    Pre Ejercicio

--- Clase12-Websocket/src/public/client.socket.js
+++ Clase12-Websocket/src/public/client.socket.js
@@ -6,4 +6,17 @@ const messagePool = document.querySelector('#messagePool')
 
 formMessage.addEventListener('submit', event => {
     event.preventDefault();
+
+    const username = usernameInput.value;
+    const message = messageInput.value;
+
+    socket.emit('client:message', {username, message});
+})
+
+socket.on('server:message', messageArray => {
+    messagePool.innerHTML = ""
+    messageArray.forEach(messageInfo => {
+        messagePool.innerHTML += `<li>${messageInfo.username}: ${messageInfo.message}</li>`
+
+    })
 })
--- Clase12-Websocket/src/server.js
+++ Clase12-Websocket/src/server.js
@@ -6,6 +6,7 @@ const { Server: IOServer } = require('socket.io');
 const expressServer = app.listen(port, ()=> {
     console.log(`Server running on port ${port}`);
 })
+const messageArray = [];
 
 app.use(express.static(path.join(__dirname, './public')));
 
@@ -13,4 +14,12 @@ const io = new IOServer(expressServer);
 
 io.on('connection', socket => {
     console.log(`Se conectó el id ${socket.id}`)
+
+    socket.emit('server:message', messageArray);
+
+    socket.on('client:message', messageInfo => {
+        messageArray.push(messageInfo)
+
+        io.emit('server:message', messageArray);
+    })
 })
